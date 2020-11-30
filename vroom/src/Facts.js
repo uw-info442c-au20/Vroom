8c1c3ab870efcf307a998da867c5e6ade3af8959
Ryan <dangryan@uw.edu>
2020-11-30 11:55:12 -0800

    Added content

--- vroom/src/Facts.js
+++ vroom/src/Facts.js
@@ -4,9 +4,10 @@ const home = () => {
     return (
        <div>
           <h1>Facts about Carbon Emissions</h1>
+            <h2>What are carbon emissions?</h2>
            <p>Facts body content</p>
        </div>
     );
 }
- 
+
 export default home;
