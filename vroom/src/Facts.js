7d290c2f14d0393ea9430f858f7212e68c0ee27f
Ryan <dangryan@uw.edu>
2020-12-06 14:41:13 -0800

    adding more content

--- vroom/src/Facts.js
+++ vroom/src/Facts.js
@@ -11,6 +11,23 @@ const Facts = () => {
                     States Environmental Protection Agency, transportation generates the largest share of
                     Greenhouse gas emissions.
                 </p>
+
+            <h2>Why should we be concerned with carbon emissions?</h2>
+                <p>
+                    Carbon emissions are a form of greenhouse gas that traps heat within the Earth’s 
+                    atmosphere and causes global temperatures to rise. According to studies, the largest 
+                    source of greenhouse gas emissions have been caused by human activities. This includes 
+                    the burning of fossil fuels for electricity, heat, and transportation.
+                </p>
+            
+            <h2>How can we reduce our carbon footprint?</h2>
+                <p>
+                A carbon footprint is the total amount of greenhouse gas emissions that come from the production 
+                and usage of a product or service.This includes carbon dioxide which is a gas that is most commonly 
+                emitted by humans. The bulk of an individual’s carbon footprint will come from transportation, housing, 
+                and food. While real solutions require actions on a global scale, there are choices that you can make in your 
+                everyday life to lessen your personal impact on the environment.
+                </p>
        </div>
     );
 }
